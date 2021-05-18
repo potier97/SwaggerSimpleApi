9da3c1173d02b88c0a2b98accd3227a0fabd8aa8
Nicolás Potier <nicolaspotier97@gmail.com>
2021-05-17 23:33:19 -0500

    fix index.html

--- src/app.ts
+++ src/app.ts
@@ -10,16 +10,20 @@ import path from 'path'
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
+console.log(__dirname)
 
-app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'html');
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')))
+ 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 const specs = swaggerJsdoc(options)
 app.use(tasksRoutes);
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs));
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('/', (req, res) =>  res.sendFile(path.join(__dirname, 'public\index.html')))
 
 export default app
